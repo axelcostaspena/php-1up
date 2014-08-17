d5942f81bbba8f7bd9f550978556b55062fd4cad
Áxel Costas Pena <axl.coding@gmail.com>
2014-08-17 18:05:29 +0200

    Add unit test class for PhpReplaceQuotesWithEscapingIntention with simple test.

---
+++ testData/com.axeldev.PhpReplaceQuotesWithEscapingIntentionTest/beforeSimpleString.php
@@ -0,0 +1,3 @@
+<?
+
+$foo = 'foo <caret>bar';
---
+++ testSrc/com/axeldev/PhpReplaceQuotesWithEscapingIntentionTest.java
@@ -0,0 +1,57 @@
+package com.axeldev;
+
+import com.axeldev.php.lang.intentions.PhpReplaceQuotesWithEscapingIntention;
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * Created by asdf
+ */
+public class PhpReplaceQuotesWithEscapingIntentionTest extends LightPlatformCodeInsightFixtureTestCase {
+
+    public static final String TEST_INTENTION_NAME = PhpReplaceQuotesWithEscapingIntention.INTENTION_NAME;
+
+    private static class MyDescriptor extends DefaultLightProjectDescriptor {
+        @Override
+        public Sdk getSdk() {
+            return JavaSdk.getInstance().createJdk("1.7", new File(getProjectRootPath(), "mockJDK-1.7").getPath(), false);
+        }
+    }
+
+    @NotNull
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return new MyDescriptor();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        File sourceRoot = getProjectRootPath();
+        return new File(sourceRoot, "testData").getPath();
+    }
+
+    private static File getProjectRootPath() {
+        String testPath = PathManager.getJarPathForClass(PhpReplaceQuotesWithEscapingIntention.class);
+        return new File(testPath, "../../..");
+    }
+
+    private void basePhpIntentionTest(String testName, String intentionName) {
+        myFixture.configureByFile("before" + testName + ".php");
+        IntentionAction intention = myFixture.findSingleIntention(intentionName);
+        myFixture.launchAction(intention);
+        myFixture.checkResultByFile("after" + testName + ".php");
+    }
+
+    public void testSimpleString() {
+        basePhpIntentionTest(getTestName(false), TEST_INTENTION_NAME);
+    }
+
+}
