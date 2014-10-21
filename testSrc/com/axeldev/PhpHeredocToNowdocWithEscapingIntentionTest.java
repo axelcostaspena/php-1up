079bcbb4e8f872520d88eea6c11e13672c364fc3
Áxel Costas Pena <axl.coding@gmail.com>
2014-10-21 21:56:13 +0200

    Extract base class for tests

--- testSrc/com/axeldev/PhpHeredocToNowdocWithEscapingIntentionTest.java
+++ testSrc/com/axeldev/PhpHeredocToNowdocWithEscapingIntentionTest.java
@@ -1,50 +1,8 @@
 package com.axeldev;
 
-import com.intellij.codeInsight.intention.IntentionAction;
-import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.projectRoots.JavaSdk;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
-import org.jetbrains.annotations.NotNull;
+public class PhpHeredocToNowdocWithEscapingIntentionTest extends Php1UpLightCodeInsightFixtureTestCase {
 
-import java.io.File;
-
-public class PhpHeredocToNowdocWithEscapingIntentionTest extends LightCodeInsightFixtureTestCase {
     private static final String TEST_INTENTION_NAME = PhpHeredocToNowdocWithEscapingIntention.INTENTION_NAME;
-
-    private static class MyDescriptor extends DefaultLightProjectDescriptor {
-        @Override
-        public Sdk getSdk() {
-            return JavaSdk.getInstance().createJdk("1.7", new File(getProjectRootPath(), "mockJDK-1.7").getPath(), false);
-        }
-    }
-
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return new MyDescriptor();
-    }
-
-    @Override
-    protected String getTestDataPath() {
-        File sourceRoot = getProjectRootPath();
-        return new File(new File(sourceRoot, "testData"), getClass().getName()).getPath();
-    }
-
-    private static File getProjectRootPath() {
-        String testPath = PathManager.getJarPathForClass(PhpHeredocToNowdocWithEscapingIntention.class);
-        return new File(testPath, "../../..");
-    }
-
-    private void phpIntentionTest(String testName, String intentionName) {
-        myFixture.configureByFile("before" + testName + ".php");
-        IntentionAction intention = myFixture.getAvailableIntention(intentionName);
-        if (intention == null) throw new AssertionError("Intention \"" + intentionName + "\" is not available at specified document position");
-        myFixture.launchAction(intention);
-        myFixture.checkResultByFile("after" + testName + ".php");
-    }
 
     public void testIntentionDescriptionExample() {
         phpIntentionTest(getTestName(false), TEST_INTENTION_NAME);
--- testSrc/com/axeldev/PhpReplaceDoubleQuotesWithEscapingIntentionTest.java
+++ testSrc/com/axeldev/PhpReplaceDoubleQuotesWithEscapingIntentionTest.java
@@ -1,51 +1,9 @@
 package com.axeldev;
 
-import com.intellij.codeInsight.intention.IntentionAction;
-import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.projectRoots.JavaSdk;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
-import org.jetbrains.annotations.NotNull;
+public class PhpReplaceDoubleQuotesWithEscapingIntentionTest extends Php1UpLightCodeInsightFixtureTestCase {
 
-import java.io.File;
-
-public class PhpReplaceDoubleQuotesWithEscapingIntentionTest extends LightCodeInsightFixtureTestCase {
     private static final String TEST_INTENTION_NAME_NO_VARS       = PhpReplaceDoubleQuotesWithEscapingIntention.INTENTION_NAME_NO_VARS;
     private static final String TEST_INTENTION_NAME_EMBEDDED_VARS = PhpReplaceDoubleQuotesWithEscapingIntention.INTENTION_NAME_EMBEDDED_VARS;
-
-    private static class MyDescriptor extends DefaultLightProjectDescriptor {
-        @Override
-        public Sdk getSdk() {
-            return JavaSdk.getInstance().createJdk("1.7", new File(getProjectRootPath(), "mockJDK-1.7").getPath(), false);
-        }
-    }
-
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return new MyDescriptor();
-    }
-
-    @Override
-    protected String getTestDataPath() {
-        File sourceRoot = getProjectRootPath();
-        return new File(new File(sourceRoot, "testData"), getClass().getName()).getPath();
-    }
-
-    private static File getProjectRootPath() {
-        String testPath = PathManager.getJarPathForClass(PhpReplaceDoubleQuotesWithEscapingIntention.class);
-        return new File(testPath, "../../..");
-    }
-
-    private void phpIntentionTest(String testName, String intentionName) {
-        myFixture.configureByFile("before" + testName + ".php");
-        IntentionAction intention = myFixture.getAvailableIntention(intentionName);
-        if (intention == null) throw new AssertionError("Intention \"" + intentionName + "\" is not available at specified document position");
-        myFixture.launchAction(intention);
-        myFixture.checkResultByFile("after" + testName + ".php");
-    }
 
     public void testIntentionDescriptionExample() {
         phpIntentionTest(getTestName(false), TEST_INTENTION_NAME_EMBEDDED_VARS);
