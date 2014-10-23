7ca764d1c1b7d0d13408551a2e4c6139c7a8b83f
Áxel Costas Pena <axl.coding@gmail.com>
2014-10-23 23:49:14 +0200

    Add test method for checking for unavailable intentions

--- testSrc/com/axeldev/BaseFixtureTestCase.java
+++ testSrc/com/axeldev/BaseFixtureTestCase.java
@@ -35,44 +35,58 @@ public abstract class BaseFixtureTestCase extends LightCodeInsightFixtureTestCas
     public abstract String getTestDataFilesExtension();
 
     protected void checkIntentionAvailableTest(String testName, String intentionName) {
-        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, false, false);
+        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, IntentionAvailability.SingleOrMultiple, false);
     }
 
     protected void checkUniqueIntentionAvailableTest(String testName, String intentionName) {
-        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, true, false);
+        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, IntentionAvailability.Single, false);
+    }
+
+    protected void checkIntentionUnavailableTest(String testName, String intentionName) {
+        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, IntentionAvailability.Unavailable, false);
     }
 
     protected void invokeUniqueIntentionTest(String testName, String intentionName) {
-        fixtureTestCase(testName, getTestDataFilesExtension(), false, intentionName, true, true);
+        fixtureTestCase(testName, getTestDataFilesExtension(), false, intentionName, IntentionAvailability.Single, true);
     }
 
     protected void invokeIntentionTest(String testName, String intentionName) {
-        fixtureTestCase(testName, getTestDataFilesExtension(), false, intentionName, false, true);
+        fixtureTestCase(testName, getTestDataFilesExtension(), false, intentionName, IntentionAvailability.SingleOrMultiple, true);
     }
 
     protected void invokeCancelledUniqueIntentionTest(String testName, String intentionName) {
-        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, true, true);
+        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, IntentionAvailability.Single, true);
     }
 
     protected void invokeCancelledIntentionTest(String testName, String intentionName) {
-        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, false, true);
+        fixtureTestCase(testName, getTestDataFilesExtension(), true, intentionName, IntentionAvailability.SingleOrMultiple, true);
     }
 
-    private void fixtureTestCase(String testDataFilesName, String testDataFilesExtension, boolean reuseTestDataBeforeFile, String intentionName, boolean breakOnDuplicateIntention, boolean launchIntention) {
+    private void fixtureTestCase(String testDataFilesName, String testDataFilesExtension, boolean reuseTestDataBeforeFile, String intentionName, IntentionAvailability expectedIntentionAvailability, boolean launchIntention) {
         String beforeTestDataFileName = "before" + testDataFilesName + "." + testDataFilesExtension;
         myFixture.configureByFile(beforeTestDataFileName);
         List<IntentionAction> intentions = findAvailableIntentions(myFixture, intentionName);
-        if (intentions.size() == 0) {
-            throw new AssertionError("Intention \"" + intentionName + "\" is not available at specified document position");
-        }
-        if (breakOnDuplicateIntention && intentions.size() > 1) {
-            throw new AssertionError("Duplicate intention \"" + intentionName + "\" found at specified document position");
-        }
-        if (launchIntention) {
-            myFixture.launchAction(intentions.get(0));
+        if (expectedIntentionAvailability == IntentionAvailability.Unavailable) {
+            if (intentions.size() > 0) {
+                throw new AssertionError("Intention \"" + intentionName + "\" is available at specified document position");
+            }
+        } else {
+            if (intentions.size() == 0) {
+                throw new AssertionError("Intention \"" + intentionName + "\" is not available at specified document position");
+            }
+            if (expectedIntentionAvailability == IntentionAvailability.Single && intentions.size() > 1) {
+                throw new AssertionError("Duplicate intention \"" + intentionName + "\" found at specified document position");
+            }
+            if (launchIntention) {
+                myFixture.launchAction(intentions.get(0));
+            }
         }
         String afterTestDataFileName = (reuseTestDataBeforeFile ? "before" : "after") + testDataFilesName + "." + testDataFilesExtension;
         myFixture.checkResultByFile(afterTestDataFileName);
+    }
+
+    private enum IntentionAvailability {
+        Unavailable, Single, SingleOrMultiple
     }
 
     private List<IntentionAction> findAvailableIntentions(JavaCodeInsightTestFixture fixture, String intentionName) {
@@ -91,4 +105,5 @@ public abstract class BaseFixtureTestCase extends LightCodeInsightFixtureTestCas
             return JavaSdk.getInstance().createJdk("1.7", new File(getProjectRootPath(), "mockJDK-1.7").getPath(), false);
         }
     }
+
 }
